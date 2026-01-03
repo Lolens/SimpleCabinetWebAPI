99abbdb3192be52f9df081ed9f68a58c4173bd22
Illia <75110249+Lolens@users.noreply.github.com>
2026-01-03 16:08:54 +0300

    Add files via upload

---
+++ src/main/java/com/gravitlauncher/simplecabinet/web/configuration/properties/storage/WebConfig.java
@@ -0,0 +1,38 @@
+package pro.gravit.simplecabinet.web.configuration.properties.storage;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "webconfig")
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    String frontendUrl;
+    final FileStorageConfig storageConfig;
+
+    public WebConfig(FileStorageConfig storageConfig) {
+        this.storageConfig = storageConfig;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("file:" + storageConfig.getLocalPath() + "/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/assets/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods("GET")
+                .allowCredentials(true);
+    }
+}
